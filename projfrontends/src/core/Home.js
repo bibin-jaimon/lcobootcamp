0704945b41dfeaceda83aed3156269d9eef74852
bibinjaimon <bibinj@qburst.com>
2020-11-04 23:57:50 +0530

    Added gitignore

---
+++ projfrontends/src/core/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles.css";
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-white">Hello world</h1>
+    </div>
+  );
+};
+export default Home;
